cce2c9a981633283fb14a9fee6fc346a595b557b
sourcepirate <sathyanarrayanan@yandex.ocm>
2020-01-19 23:21:47 +0800

    updated lib

--- raptor/__init__.py
+++ raptor/__init__.py
@@ -3,3 +3,8 @@
 __author__ = """sourcepirate"""
 __email__ = "sourcepirate@codelantics.com"
 __version__ = "0.1.0"
+
+from .datasource import *
+from .transform import *
+from .registry import *
+from .reports import *
